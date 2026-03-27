71133e0d744da9c94f6cff363b82183e118aaf51
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-03-27 13:27:38 +0300

    Update weapon_data.gd

--- core/weapon/weapon_data.gd
+++ core/weapon/weapon_data.gd
@@ -3,12 +3,12 @@ class_name WeaponData
 
 enum WeaponType { MELEE, RANGED }
 @export var weapon_type: WeaponType = WeaponType.MELEE
-
 @export var weapon_name:String="unknown"
 @export var weapon_category:String="unknown"
-@export var weapon_type:WeaponType=WeaponType.MELEE
 @export var damage:float=3.0
 @export var cooldown_time:float=1.0
 @export var attack_time:float=1.0
 @export var range:float=2.0
 @export var knockback_force:float=0.0
+@export var damage_type:String="default"
+@export var adv_damage_type:String="none"
