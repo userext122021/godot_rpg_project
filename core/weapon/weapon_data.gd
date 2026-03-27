acd405ac023d737fd1278ea4751647849a2fddb6
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-03-27 13:26:11 +0300

    Update weapon_data.gd

--- core/weapon/weapon_data.gd
+++ core/weapon/weapon_data.gd
@@ -1,10 +1,12 @@
 extends Resource
 class_name WeaponData
 
+enum WeaponType { MELEE, RANGED }
+@export var weapon_type: WeaponType = WeaponType.MELEE
 
 @export var weapon_name:String="unknown"
 @export var weapon_category:String="unknown"
-@export var weapon_type:String="melee" #melee,ranged
+@export var weapon_type:WeaponType=WeaponType.MELEE
 @export var damage:float=3.0
 @export var cooldown_time:float=1.0
 @export var attack_time:float=1.0
