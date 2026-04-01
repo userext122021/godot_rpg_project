765aeea9a6185b7a5b748de7a5d064cb549949da
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-01 11:30:46 +0300

    Update stats_control.gd

--- core/entities/stats_control.gd
+++ core/entities/stats_control.gd
@@ -5,9 +5,6 @@ signal died
 signal damage_taken(damage:float)
 	
 @export var data:StatsData
-var status_timers={}
-var status_total_timers={}
-
 func calculate_damage(damage,damage_type) -> float:
 	return damage
 
@@ -27,21 +24,6 @@ func update(delta:float):
 	regen(delta)
 	pass
 	
-func update_statuses(delta:float):
-	if status_timers.is_empty():
-		return
-	
-	pass
-
-func apply_effect(effect_name:String):
-	if not data.statuses.has(effect_name):
-		return
-	var sd:StatusEffectData=data.statuses[effect_name]
-	status_timers[effect_name]=0
-	status_total_timers[effect_name]=0
-	
-func update_states(delta:float):
-	pass
 func regen(delta):
 	if data.hp<data.max_hp:
 		data.hp+=data.regen_hp_per_second*delta
