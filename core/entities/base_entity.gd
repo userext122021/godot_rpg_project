ffe5274f13028dd41da010d74cdbfbebb5edc673
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-01 09:46:11 +0300

    Update base_entity.gd

--- core/entities/base_entity.gd
+++ core/entities/base_entity.gd
@@ -65,13 +65,12 @@ func update_state():
 		state_changed.emit(new_state,current_state)
 		current_state=new_state
 
-func take_damage(damage:float) -> float:
-	stats.take_damage(damage)
-	return damage
+func take_damage(damage:float,damage_type:String="physical") -> float:
+	return stats.take_damage(damage,damage_type)
 
 func take_hit(weapon_data:WeaponData,attacker_position:Vector3) -> float:
 	#process other hit parameters
-	return take_damage(weapon_data.damage)
+	return take_damage(weapon_data.damage,weapon_data.damage_type)
 
 
 func attack():
