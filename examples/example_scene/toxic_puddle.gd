1d5f850899c3e5c90215a0dc4bfd60bb9e0583fd
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-01 16:25:54 +0300

    Update toxic_puddle.gd

--- examples/example_scene/toxic_puddle.gd
+++ examples/example_scene/toxic_puddle.gd
@@ -1,7 +1,11 @@
 extends DangerZone
 class_name ToxicPuddle
+var effect_name:String="poison"
+var duration:float=5.0
+var tick_time:float=1.0
+var damage:float=3
 
 func apply_effect(body:Node3D):
-	if body.has_method("take_damage"):
-		body.take_damage(damage)
-	print("TOXIC PUDDLE: apply on ",body)
+	if body.has_method("add_effect"):
+		body.add_effect(effect_name,duration,tick_time,damage)
+		print("TOXIC PUDDLE: apply on ",body)
