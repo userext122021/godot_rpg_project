bd32294f7cec3e21ad5f838f9631f41eab1f84c8
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-01 11:59:49 +0300

    Delete core/entities/status_effect.gd

--- core/entities/status_effect.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class_name StatusEffect
-
-@export var status_name:String="unknown"
-@export var duration:float=3.0
-@export var interval:float=1.0
-@export var damage:float=5.0
-@export var total_timer:float=0.0
-@export var tick_timer:float=0.0
