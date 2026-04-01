e0b3282718e80fdaa59f3a52c2d55f298395cfac
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-01 08:00:11 +0300

    Улучшил лужу (dangerzone)

--- core/danger_zone/danger_zone.gd
+++ core/danger_zone/danger_zone.gd
@@ -4,25 +4,43 @@ class_name DangerZone
 signal entity_entered(zone_type:String,entity:BaseEntity)
 signal entity_exited(zone_type:String,entity:BaseEntity)
 
-
+var entities=[]
 @export var size:Vector3=Vector3(4,4,4)
 @export var zone_type:String="unknown"
-
+@export var update_interval:float=3.0
+@export var damage:float=7.0
+var update_timer:float=0
 func _ready() -> void:
-	update()
+	pass
 
-func update():
-	var c:CylinderShape3D=$CollisionShape3D.shape
-	c.radius=max(size.x,size.z)
+func apply_effect(body:Node3D):
+	print("DEBUG: poisoning ",body)
+	pass
+	
+func _process(delta: float) -> void:
+	update_timer-=delta
+	if update_timer<=0:
+		update_timer=update_interval
+		if entities.is_empty():
+			return
+		for e in entities:
+			apply_effect(e)
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.has_node("StatsControl"):
+		if not entities.has(body):
+			entities.append(body)
 		entity_entered.emit(zone_type,body)
 	pass # Replace with function body.
 
 
 func _on_body_exited(body: Node3D) -> void:
 	if body.has_node("StatsControl"):
+		if entities.has(body):
+			for i in range(entities.size()):
+				if entities[i]==body:
+					entities.remove_at(i)
+	
 		entity_exited.emit(zone_type,body)
 	pass # Replace with function body.
--- examples/example_scene/toxic_puddle.gd
+++ examples/example_scene/toxic_puddle.gd
@@ -1,24 +1,7 @@
-extends MeshInstance3D
+extends DangerZone
+class_name ToxicPuddle
 
-@export var damage:float=7.0
-var entities=[]
-
-func _on_danger_zone_entity_entered(zone_type: String, entity: BaseEntity) -> void:
-	entities.push_back(entity)
-	pass # Replace with function body.
-
-
-func _on_danger_zone_entity_exited(zone_type: String, entity: BaseEntity) -> void:
-	if entities.has(entity):
-		for i in range(entities.size()):
-			if entities[i]==entity:
-				entities.remove_at(i)
-				return
-	pass # Replace with function body.
-
-
-func _on_timer_timeout() -> void:
-	for e in entities:
-		if e.has_method("take_damage"):
-			e.take_damage(damage)
-	pass # Replace with function body.
+func apply_effect(body:Node3D):
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	print("TOXIC PUDDLE: apply on ",body)
