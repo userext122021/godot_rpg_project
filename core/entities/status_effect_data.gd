9d47b7b0bfbf49264c561d5e60c1582f33d879c6
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-01 11:51:40 +0300

    Update status_effect_data.gd

--- core/entities/status_effect_data.gd
+++ core/entities/status_effect_data.gd
@@ -1,7 +1,9 @@
-extends Resource
-class_name StatusEffectData
+extends Node
+class_name StatusEffect
 
 @export var status_name:String="unknown"
 @export var duration:float=3.0
 @export var interval:float=1.0
 @export var damage:float=5.0
+@export var total_timer:float=0.0
+@export var tick_timer:float=0.0
