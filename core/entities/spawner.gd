3499cdd258d973b84758d4f69eac369ad829892c
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-02 15:40:06 +0300

    обновил спавнеры

--- core/entities/spawner.gd
+++ core/entities/spawner.gd
@@ -1,6 +1,7 @@
 extends Area3D
 class_name Spawner
-
+signal object_spawned(object_body:Node3D)
+@export var spawner_name:String="unknown_spawner"
 @export var radius:float=10.0
 @export var is_active:bool=true
 @export var spawn_interval:float=10.0
@@ -11,10 +12,24 @@ var player_body:Node3D=null
 var spawner_timer:float=0
 
 func _ready() -> void:
-	$Timer.wait_time=spawn_interval
-	var shape:CylinderShape3D=$CollisionShape3D.shape
-	shape.radius=radius
+	var c:CylinderShape3D=$CollisionShape3D.shape.duplicate()
+	c.radius=radius	
+	$CollisionShape3D.shape=c
+	pass
 
+	
+func _process(delta: float) -> void:
+	if not is_active:
+		return
+	if spawned_object:
+		return
+	if not player_body:
+		return
+	spawner_timer-=delta
+	if spawner_timer<=0:
+		spawner_timer=spawn_interval
+		spawn()
+		 
 func spawn():
 	if randf()>probability:
 		return
@@ -23,7 +38,6 @@ func spawn():
 	if not player_body:
 		return
 	if global_position.distance_to(player_body.global_position)>radius:
-		#is_active=false
 		player_body=null
 		return
 	if spawned_object:
@@ -36,41 +50,30 @@ func spawn():
 	add_child(spawned_object)
 	spawned_object.global_position=global_position
 	spawned_object.tree_exited.connect(_on_object_tree_exited)
-	$Timer.stop()
+	object_spawned.emit(spawned_object)
 	pass
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if not is_active:
 		return
+	print("Body entered ",body)
 	if body.has_node("StatsControl"):
 		var sc:StatsControl=body.get_node("StatsControl")
 		if sc.data.entity_category=="player":
-			#is_active=true
+			spawner_timer=0
 			player_body=body
-			$Timer.start()
 	pass # Replace with function body.
 
 
 func _on_body_exited(body: Node3D) -> void:
-	if body.has_node("StatsControl"):
-		var sc:StatsControl=body.get_node("StatsControl")
-		if sc.data.entity_category=="player":
-			#is_active=false
-			player_body=null
-			$Timer.stop()
+	if body==player_body:
+		player_body=null
 	pass # Replace with function body.
 
 
-func _on_timer_timeout() -> void:
-	if not is_active:
-		$Timer.stop()
-		return
-	spawn()
-	pass # Replace with function body.
+
 
 
 func _on_object_tree_exited():
 	spawned_object=null
-	if is_active:
-		$Timer.start()
