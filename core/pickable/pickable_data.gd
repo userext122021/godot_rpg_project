5425fdec2a515f14e117eeda14f01289cd0130eb
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-04-07 00:09:31 +0300

    очистка

--- core/pickable/pickable_data.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name PickableData
-
-
-@export var pickable_name:String="unknown_pickable"
-@export var pickable_category:String="unknown_pickable_category"
-@export var is_usable:bool=true
